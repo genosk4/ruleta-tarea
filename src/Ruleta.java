61f2d109510c088f578f36dba180e8aaaf1ba371
genosk4 <lincopidaniel7@gmail.com>
2025-08-26 02:27:02 -0400

    opcion 1 añadida

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Ruleta {
 
+    public static Random rng = new Random();
+
+
     public static void main(String[] args) {
         menu();
     }
@@ -31,9 +34,31 @@ public class Ruleta {
 
     public static void ejecutarOpcion(int opcion, Scanner in) {
         switch (opcion) {
+            case 1 -> iniciarRonda(in);
+
             case 3 -> System.out.println("Gracias por jugar...");
             default -> System.out.println("Selecciona una opcion válida.");
         }
+    }
+    public static char leerTipoApuesta(Scanner in) {
+        char tipo;
+        do {
+            System.out.println("Elige tipo de apuesta (R=rojo, N=negro, P=par, I=impar)");
+            tipo = in.next().toUpperCase().charAt(0);
+        } while (tipo != 'R' && tipo != 'N' && tipo != 'P' && tipo != 'I');
+        return tipo;
+    }
+
+    public static void iniciarRonda(Scanner in) {
+        char tipo = leerTipoApuesta(in);
+        System.out.println("Ingresa el monto apostado: ");
+        int monto = in.nextInt();
+
+
+
+    }
+    public static int girarRuleta() {
+        return rng.nextInt(37);
     }
 
 
