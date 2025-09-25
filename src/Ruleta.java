c07c167e5de060535f07b4e39f20655f19844c95
genosk4 <lincopidaniel7@gmail.com>
2025-09-25 13:37:00 -0300

    Clase Ruleta modificada para que pueda funcionar con una ventana

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -1,85 +1,34 @@
-
 import java.util.Random;
-import java.util.Scanner;
 
 public class Ruleta {
 
     public static final int MAX_HISTORIAL = 100;
-    public static int[] historialNumeros = new int[MAX_HISTORIAL];
-    public static int[] historialApuestas = new int[MAX_HISTORIAL];
-    public static boolean[] historialAciertos = new boolean[MAX_HISTORIAL];
-    public static int historialSize = 0;
-    public static Random rng = new Random();
-    public static int[] numerosRojos =
+    private static final int[] historialNumeros = new int[MAX_HISTORIAL];
+    private static final int[] historialApuestas = new int[MAX_HISTORIAL];
+    private static final char[] historialTipos = new char[MAX_HISTORIAL];
+    private static final boolean[] historialAciertos = new boolean[MAX_HISTORIAL];
+    private static int historialSize = 0;
+
+    private static final Random rng = new Random();
+
+    private static final int[] numerosRojos =
             {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
 
-
-    public static void main(String[] args) {
-        menu();
-    }
-
-    public static void menu() {
-        Scanner in = new Scanner(System.in);
-        int opcion;
-        do {
-            mostrarMenu();
-            opcion = leerOpcion(in);
-            ejecutarOpcion(opcion, in);
-        } while (opcion != 3);
-        in.close();
-    }
-
-    public static void mostrarMenu() {
-        System.out.println(" Menú");
-        System.out.println("1.- Iniciar ronda de ruleta");
-        System.out.println("2.- Ver estadisticas ");
-        System.out.println("3.- Salir");
-        System.out.println("Elige una opcion: ");
-    }
-    public static int leerOpcion(Scanner in) {
-        return in.nextInt();
-    }
-
-    public static void ejecutarOpcion(int opcion, Scanner in) {
-        switch (opcion) {
-            case 1 -> iniciarRonda(in);
-            case 2 -> mostrarEstadisticas();
-            case 3 -> System.out.println("Gracias por jugar...");
-            default -> System.out.println("Selecciona una opcion válida.");
-        }
-    }
-    public static char leerTipoApuesta(Scanner in) {
-        char tipo;
-        do {
-            System.out.println("Elige tipo de apuesta (R=rojo, N=negro, P=par, I=impar)");
-            tipo = in.next().toUpperCase().charAt(0);
-        } while (tipo != 'R' && tipo != 'N' && tipo != 'P' && tipo != 'I');
-        return tipo;
-    }
-
-    public static void iniciarRonda(Scanner in) {
-        char tipo = leerTipoApuesta(in);
-        System.out.println("Ingresa el monto apostado: ");
-        int monto = in.nextInt();
-
-        int numero = girarRuleta();
-        boolean acierto = evaluarResultado(numero, tipo);
-
-        registrarResultado(numero, monto, acierto);
-        mostrarResultado(numero, tipo, monto, acierto);
-    }
     public static int girarRuleta() {
         return rng.nextInt(37);
     }
 
+
     public static boolean esRojo(int n) {
         for (int rojo : numerosRojos) {
             if (rojo == n) return true;
-        } return false;
+        }
+        return false;
     }
 
+
     public static boolean evaluarResultado(int numero, char tipo) {
-        switch (tipo) {
+        switch (Character.toUpperCase(tipo)) {
             case 'R': return esRojo(numero);
             case 'N': return numero != 0 && !esRojo(numero);
             case 'P': return numero != 0 && numero % 2 == 0;
@@ -88,30 +37,23 @@ public class Ruleta {
         }
     }
 
-    public static void registrarResultado(int numero, int apuesta, boolean acierto) {
+    public static void registrarResultado(int numero, char tipo, int apuesta, boolean acierto) {
         if (historialSize < MAX_HISTORIAL) {
             historialNumeros[historialSize] = numero;
+            historialTipos[historialSize] = tipo;
             historialApuestas[historialSize] = apuesta;
             historialAciertos[historialSize] = acierto;
             historialSize++;
         }
     }
 
-    public static void mostrarResultado(int numero, char tipo, int monto,boolean acierto) {
-        System.out.println("Numero obtenido: " + numero);
-        System.out.println("Tu apuesta fue: " + tipo + " con monto $" + monto);
-        if (acierto) {
-            System.out.println("Ganaste");
-        } else {
-            System.out.println("Perdiste");
-        }
-    }
 
-    public static void mostrarEstadisticas() {
+    public static String getEstadisticas() {
         int totalJugadas = historialSize;
         int totalApostado = 0;
         int totalAciertos = 0;
         int ganancia = 0;
+
         for (int i = 0; i < historialSize; i++) {
             totalApostado += historialApuestas[i];
             if (historialAciertos[i]) {
@@ -121,18 +63,26 @@ public class Ruleta {
                 ganancia -= historialApuestas[i];
             }
         }
-        System.out.println("----- Estadisticas -----");
-        System.out.println("Cantidad de rondas: " + totalJugadas);
-        System.out.println("Total apostado: $" + totalApostado);
-        System.out.println("Total aciertos: " + totalAciertos);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("----- Estadísticas -----\n");
+        sb.append("Cantidad de rondas: ").append(totalJugadas).append("\n");
+        sb.append("Total apostado: $").append(totalApostado).append("\n");
+        sb.append("Total aciertos: ").append(totalAciertos).append("\n");
         if (totalJugadas > 0) {
             double porcentaje = (totalAciertos * 100.0) / totalJugadas;
-            System.out.println("% de acierto: " + String.format("%.2f", porcentaje) + "%");
+            sb.append("% de acierto: ").append(String.format("%.2f", porcentaje)).append("%\n");
         }
-        System.out.println("Ganancia/Pérdida neta: $" + ganancia);
+        sb.append("Ganancia/Pérdida neta: $").append(ganancia).append("\n");
+
+        return sb.toString();
     }
 
 
+    public static int getHistorialSize() { return historialSize; }
 
-
+    public static int getNumeroHistorial(int i) { return historialNumeros[i]; }
+    public static char getTipoHistorial(int i) { return historialTipos[i]; }
+    public static int getApuestaHistorial(int i) { return historialApuestas[i]; }
+    public static boolean getAciertoHistorial(int i) { return historialAciertos[i]; }
 }
